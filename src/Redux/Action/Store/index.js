78154768ce306bfda0ca104a4300a341ee97ec30
Monika123-max <58550056+Monika123-max@users.noreply.github.com>
2022-05-16 01:11:43 +0530

    removed folder extra named store

--- src/Redux/Action/Store/index.js
+++
@@ -1,7 +0,0 @@
-import reducer from "../../Reducer/index";
-import { createStore } from "redux";
-
-export const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
